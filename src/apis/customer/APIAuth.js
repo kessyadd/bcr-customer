a1d9019233cfea45dbda752d43f194f05af84fa2
kessyadd <kessya.025@gmail.com>
2023-01-08 21:41:14 +0700

    setup API, configs, and utils

---
+++ src/apis/customer/APIAuth.js
@@ -0,0 +1,19 @@
+import axiosCustomerInstance from "../../configs/axiosInstance";
+import Auth from "../../utils/Auth";
+
+const APIAuth = {
+  login: async ({ email, password }) => {
+    try {
+      const res = await axiosCustomerInstance.post("/auth/login", {
+        email,
+        password,
+      });
+      Auth.storeUserInfoToCookies(res.data.access_token);
+      return res.data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+};
+
+export default APIAuth;
