5082dbc192180f157f2f7dc60f258d643e885e83
najottama <najottama@gmail.com>
2023-01-20 22:19:54 +0700

    update ui
    update ui

--- src/components/ChoosePayment.jsx
+++ src/components/ChoosePayment.jsx
@@ -3,8 +3,12 @@ import ListGroup from "react-bootstrap/ListGroup";
 
 const ChoosePayment = () => {
   return (
-    <div>
+    <div className="m-5">
       <ListGroup>
+        <ListGroup.Item disabled variant="light">
+          <h5 className="fs-5 fw-bold">Pilih Bank Transfer</h5>
+          <h5 className="fs-6">Kamu bisa membayar dengan transfer melalui ATM, Internet Banking atau Mobile Banking</h5>
+        </ListGroup.Item>
         <ListGroup.Item action variant="light">
           BCA Transfer
         </ListGroup.Item>
@@ -14,6 +18,12 @@ const ChoosePayment = () => {
         <ListGroup.Item action variant="light">
           Mandiri Transfer
         </ListGroup.Item>
+      </ListGroup>
+      <ListGroup variant="flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
       </ListGroup>
     </div>
   );
