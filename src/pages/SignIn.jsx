51b9bc7ae1e24b3678684bdf930b704b62cd00e3
kessyadd <kessya.025@gmail.com>
2023-02-08 17:59:57 +0700

    update setup route, create layout

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Col, Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "../assets/css/signIn.css";
 import APIAuth from "../apis/customer/APIAuth";
-
 import GrayButton from "../assets/img/rectangle.png";
 
 const SignIn = () => {
   const navigate = useNavigate();
+  const { search } = useLocation();
   const onFinish = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
@@ -23,21 +23,31 @@ const SignIn = () => {
     const handleSubmit = async () => {
       try {
         await APIAuth.login(payload);
+        alert("Login Success!");
         let returnTo = "/";
+        const params = new URLSearchParams(search);
+        const redirectTo = params.get("return_to");
+        if (redirectTo) returnTo += `${redirectTo}`;
         setTimeout(() => {
           navigate(returnTo);
-        }, 2000);
-      } catch (error) {}
+        }, 1000);
+      } catch (error) {
+        const err = new Error(error);
+        console.log(err);
+      }
     };
     handleSubmit();
-    console.log(e);
   };
 
   return (
     <>
       <Container className="container-fluid">
         <Row>
-          <Col lg={6} className="sign-in p-5 justify-items-center" style={{ display: "flex", alignItems: "center" }}>
+          <Col
+            lg={6}
+            className="sign-in p-5 justify-items-center"
+            style={{ display: "flex", alignItems: "center" }}
+          >
             <Row>
               <Col lg={12}>
                 <img src={GrayButton} alt="gray" />
@@ -49,19 +59,33 @@ const SignIn = () => {
                 <Form onSubmit={onFinish}>
                   <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" name="email" placeholder="Contoh: johndoe@gmail.com" />
+                    <Form.Control
+                      type="email"
+                      name="email"
+                      placeholder="Contoh: johndoe@gmail.com"
+                    />
                   </Form.Group>
                   <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name="password" placeholder="6+ karakter" />
+                    <Form.Control
+                      type="password"
+                      name="password"
+                      placeholder="6+ karakter"
+                    />
                   </Form.Group>
                   <div className="d-grid">
-                    <Button className="button-register" variant="primary" type="submit" size="lg">
+                    <Button
+                      className="button-register"
+                      variant="primary"
+                      type="submit"
+                      size="lg"
+                    >
                       Sign In
                     </Button>
                   </div>
                   <h6 className="mt-4 fw-bolder">
-                    Don't have an account? <a href="/sign-up">Sign Up for free</a>
+                    Don't have an account?{" "}
+                    <a href="/sign-up">Sign Up for free</a>
                   </h6>
                 </Form>
               </Col>
