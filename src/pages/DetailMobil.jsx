ffe4ba7a6698608c25a39bc46d9e4e6f8f59611a
kessyadd <kessya.025@gmail.com>
2023-02-17 03:44:22 +0700

    remove unused dependencies

--- src/pages/DetailMobil.jsx
+++ src/pages/DetailMobil.jsx
@@ -2,7 +2,6 @@ import SecHero from "../components/SecHero";
 import FormHasilPencarian from "../components/FormHasilPencarian";
 import DeskripsiMobil from "../components/DeskripsiMobil";
 import "../assets/css/formHasilPencarian.css";
-import { useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import CustomHemlet from "../components/CustomHelmet";
 
